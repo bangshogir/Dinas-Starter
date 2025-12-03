882e6a7da87dce0ad51143c55a935faad565307d
unknown <bangshogir@gmail.com>
2025-12-03 08:22:10 +0800

    Update admin sidebar: remove dashboard submenu and replace calendar with profil dinas

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -38,10 +38,8 @@
                     <!-- Menu Item Dashboard -->
                     <li>
                         <a href="{{ route('admin.dashboard') }}"
-                            @click.prevent="selected = (selected === 'Dashboard' ? '':'Dashboard')"
-                            class="menu-item group"
-                            :class=" (selected === 'Dashboard') || (page === 'ecommerce' || page === 'analytics' || page === 'marketing' || page === 'crm' || page === 'stocks') ? 'menu-item-active' : 'menu-item-inactive'">
-                            <svg :class="(selected === 'Dashboard') || (page === 'ecommerce' || page === 'analytics' || page === 'marketing' || page === 'crm' || page === 'stocks') ? 'menu-item-icon-active'  :'menu-item-icon-inactive'"
+                            class="menu-item group {{ request()->routeIs('admin.dashboard') ? 'menu-item-active' : 'menu-item-inactive' }}">
+                            <svg class="{{ request()->routeIs('admin.dashboard') ? 'menu-item-icon-active' : 'menu-item-icon-inactive' }}"
                                 width="24" height="24" viewBox="0 0 24 24" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" clip-rule="evenodd"
@@ -52,52 +50,28 @@
                             <span class="menu-item-text" :class="sidebarToggle ? 'lg:hidden' : ''">
                                 Dashboard
                             </span>
-
-                            <svg class="menu-item-arrow"
-                                :class="[(selected === 'Dashboard') ? 'menu-item-arrow-active' : 'menu-item-arrow-inactive', sidebarToggle ? 'lg:hidden' : '' ]"
-                                width="20" height="20" viewBox="0 0 20 20" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path d="M4.79175 7.39584L10.0001 12.6042L15.2084 7.39585" stroke="" stroke-width="1.5"
-                                    stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
                         </a>
-
-                        <!-- Dropdown Menu Start -->
-                        <div class="overflow-hidden transform translate"
-                            :class="(selected === 'Dashboard') ? 'block' :'hidden'">
-                            <ul :class="sidebarToggle ? 'lg:hidden' : 'flex'"
-                                class="flex flex-col gap-1 mt-2 menu-dropdown pl-9">
-                                <li>
-                                    <a href="{{ route('admin.dashboard') }}" class="menu-dropdown-item group"
-                                        :class="page === 'ecommerce' ? 'menu-dropdown-item-active' : 'menu-dropdown-item-inactive'">
-                                        eCommerce
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- Dropdown Menu End -->
                     </li>
                     <!-- Menu Item Dashboard -->
 
-                    <!-- Menu Item Calendar -->
+                    <!-- Menu Item Profil Dinas -->
                     <li>
-                        <a href="#" @click="selected = (selected === 'Calendar' ? '':'Calendar')"
-                            class="menu-item group"
-                            :class=" (selected === 'Calendar') && (page === 'calendar') ? 'menu-item-active' : 'menu-item-inactive'">
-                            <svg :class="(selected === 'Calendar') && (page === 'calendar') ? 'menu-item-icon-active'  :'menu-item-icon-inactive'"
+                        <a href="#"
+                            class="menu-item group {{ request()->is('admin/profil-dinas*') ? 'menu-item-active' : 'menu-item-inactive' }}">
+                            <svg class="{{ request()->is('admin/profil-dinas*') ? 'menu-item-icon-active' : 'menu-item-icon-inactive' }}"
                                 width="24" height="24" viewBox="0 0 24 24" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M8 2C8.41421 2 8.75 2.33579 8.75 2.75V3.75H15.25V2.75C15.25 2.33579 15.5858 2 16 2C16.4142 2 16.75 2.33579 16.75 2.75V3.75H18.5C19.7426 3.75 20.75 4.75736 20.75 6V9V19C20.75 20.2426 19.7426 21.25 18.5 21.25H5.5C4.25736 21.25 3.25 20.2426 3.25 19V9V6C3.25 4.75736 4.25736 3.75 5.5 3.75H7.25V2.75C7.25 2.33579 7.58579 2 8 2ZM8 5.25H5.5C5.08579 5.25 4.75 5.58579 4.75 6V8.25H19.25V6C19.25 5.58579 18.9142 5.25 18.5 5.25H16H8ZM19.25 9.75H4.75V19C4.75 19.4142 5.08579 19.75 5.5 19.75H18.5C18.9142 19.75 19.25 19.4142 19.25 19V9.75Z"
+                                    d="M4 4C4 2.89543 4.89543 2 6 2H18C19.1046 2 20 2.89543 20 4V22H4V4ZM6 4H18V20H6V4ZM8 7H11V10H8V7ZM13 7H16V10H13V7ZM8 12H11V15H8V12ZM13 12H16V15H13V12Z"
                                     fill="" />
                             </svg>
 
                             <span class="menu-item-text" :class="sidebarToggle ? 'lg:hidden' : ''">
-                                Calendar
+                                Profil Dinas
                             </span>
                         </a>
                     </li>
-                    <!-- Menu Item Calendar -->
+                    <!-- Menu Item Profil Dinas -->
                 </ul>
             </div>
         </nav>
